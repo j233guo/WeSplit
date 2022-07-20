ce24cee2b8172d70882216de40539c8a127bcc0b
Jiaming Guo <j23guo@gmail.com>
2022-07-19 23:00:00 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jiaming Guo on 2022-07-19.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
